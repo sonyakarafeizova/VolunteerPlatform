fd5d1eab3fc256e79c87741f556542af6beef2f7
Sonya Karafeizova <sonqkarafeizova@gmail.com>
2024-12-03 13:38:32 +0200

    Update project

---
+++ src/main/java/com/example/volunteerplatform/model/Availability.java
@@ -0,0 +1,28 @@
+package com.example.volunteerplatform.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Availability {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "cause_id")
+    private Cause cause;
+
+    private LocalDate date;
+    private String time;
+}
